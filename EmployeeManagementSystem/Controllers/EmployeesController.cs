451260d976687e6accfddaafe60aba04b97b8ff6
Mang Kelogs <Mang Kelogs@MANG_KELOGS>
2025-11-19 16:28:51 +0800

    add delete and edit

--- EmployeeManagementSystem/Controllers/EmployeesController.cs
+++ EmployeeManagementSystem/Controllers/EmployeesController.cs
@@ -36,6 +36,59 @@ namespace EmployeeManagementSystem.Controllers
             }
             return View(employee);
         }
+        // GET: /Employees/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+                return NotFound();
+
+            return View(employee);
+        }
+        // POST: /Employees/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Employee employee)
+        {
+            if (id != employee.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Update(employee);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(employee);
+        }
+        // GET: /Employees/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var employee = _context.Employees.Find(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return View(employee);
+        }
+
+        // POST: /Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var employee = _context.Employees.Find(id);
+
+            if (employee == null)
+                return NotFound();
+
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
 
 
     }
